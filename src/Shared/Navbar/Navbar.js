db71bc1028a74354f31624dc649dd75d280498ac
asmtanjil <asm.tanjil@gmail.com>
2022-11-24 23:02:50 +0600

    Footer Added

--- src/Shared/Navbar/Navbar.js
+++ src/Shared/Navbar/Navbar.js
@@ -10,12 +10,12 @@ const Navbar = () => {
   </React.Fragment>
 
   return (
-    <div className="navbar bg-orange-50 flex justify-between">
+    <div className="navbar bg-sky-100 flex justify-between">
       <div className="navbar-start">
         <div className="dropdown">
-          <label tabIndex={0} className="btn btn-outline lg:hidden">
+          {/* <label tabIndex={0} className="btn btn-outline lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-          </label>
+          </label> */}
           <ul tabIndex={1} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
             {menuItems}
           </ul>
@@ -27,9 +27,9 @@ const Navbar = () => {
           {menuItems}
         </ul>
       </div>
-      <label htmlFor="dashboard-drawer" tabIndex={2} className="btn btn-ghost lg:hidden">
+      {/* <label htmlFor="dashboard-drawer" tabIndex={2} className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </label>
+      </label> */}
     </div>
   );
 };
