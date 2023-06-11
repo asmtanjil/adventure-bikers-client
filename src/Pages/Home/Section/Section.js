f9696b2a1eb2136882656593720af09e3b0ab8b5
asmtanjil <asm.tanjil@gmail.com>
2023-06-11 06:44:16 +0600

    Section Added again

--- src/Pages/Home/Section/Section.js
+++ src/Pages/Home/Section/Section.js
@@ -9,7 +9,7 @@ const Section = () => {
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         <img src="https://i.ibb.co/10HF8JH/buy-4.jpg" alt="Album" className='' style={{ width: '100%' }} />
         <img src="https://i.ibb.co/wBHpCDq/buy-2.jpg" alt="Album" className='' style={{ width: '100%' }} />
-        <img src="https://i.ibb.co/HpSJzH4/buy-5.jpg" alt="Album" className='' style={{ width: '100%' }} />
+        <img src="https://i.ibb.co/10HF8JH/buy-4.jpg" alt="Album" className='' style={{ width: '100%' }} />
         <img src="https://i.ibb.co/HFw6T45/buy-3.jpg" alt="Album" className='' style={{ width: '100%' }} />
         <img src="https://i.ibb.co/HFw6T45/buy-3.jpg" alt="Album" className='' style={{ width: '100%' }} />
         <img src="https://i.ibb.co/HFw6T45/buy-3.jpg" alt="Album" className='' style={{ width: '100%' }} />
